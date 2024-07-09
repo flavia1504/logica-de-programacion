fe348d0b952b4ff566741aa1c9fb9b8297049910
flavia1504 <milhouse.mb@gmail.com>
2024-07-09 16:28:32 -0300

    logica de programacion

---
+++ app.js
@@ -0,0 +1,36 @@
+alert("hola mundo");
+let numeroSecreto = 6;
+let numeroUsuario = prompt("me indicas un numero por favor");
+console.log(numeroUsuario);
+if (numeroUsuario == numeroSecreto) {
+    alert("acertaste el numero");
+}
+
+let mensajeDeBienvenida = 'Bienvenido a la aplicación';
+alert('mensajeDeBienvenida');
+
+
+alert("¡Bienvenida y bienvenido a nuestro sitio web!");
+
+let nombre = "luna";
+let edad = 25;
+let numeroDeVentas = 25;
+let saldoDisponible = 1000;
+
+alert("¡Error! Completa todos los campos");
+
+let mensajeDeError = ("¡Error! Completa todos los campos");
+alert(mensajeDeError)
+
+
+let nombreUsuario = prompt("ingrese el nombre del usuario");
+
+let edadUsuario = prompt("ingrese su edad");
+
+let Edad = prompt("ingrese su edad");
+if (Edad >= 18) {
+    alert('¡Puede obtener su licencia de conducir!');
+}
+
+
+
